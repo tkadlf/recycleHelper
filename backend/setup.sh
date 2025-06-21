b7ff9e1e28550e9041bce35c3b5b757926fd2eed
tkadlf <137704136+tkadlf@users.noreply.github.com>
2025-06-21 21:28:33 +0900

    Update setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,2 +1,1 @@
-pip install ultralytics --no-deps
-pip install torch==1.13.1+cpu torchvision==0.14.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
